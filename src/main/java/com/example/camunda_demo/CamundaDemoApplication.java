0e024394d85109a582036adadda5e34ecc10dba7
Lars Uffmann <lars.uffmann@gmail.com>
2024-10-17 08:43:32 +0200

    initial commit

---
+++ src/main/java/com/example/camunda_demo/CamundaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.camunda_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamundaDemoApplication.class, args);
+	}
+
+}
